b1119527a5f49462257c6d8829e9a0dee54cea00
Artem Chumak <artem_chumak@hotmail.com>
2019-04-21 13:57:58 -0700

    Add project files.

---
+++ WebApplicationWinService/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebApplicationWinService.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
